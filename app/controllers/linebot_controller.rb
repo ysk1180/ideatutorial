004bb9dd150331ac6506d045195a364a7eff2ef7
Yusuke Miyazaki <ysk1180@gmail.com>
2018-06-17 14:48:17 +0900

    コントローラベース作成、ルーティング作成

---
+++ app/controllers/linebot_controller.rb
@@ -0,0 +1,51 @@
+class LinebotController < ApplicationController
+  require 'line/bot'  # gem 'line-bot-api'
+
+  # callbackアクションのCSRFトークン認証を無効
+  protect_from_forgery :except => [:callback]
+
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+
+    events = client.parse_events_from(body)
+
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          seed1 = select_word
+          seed2 = select_word
+          while seed1 == seed2
+            seed2 = select_word
+          end
+          message = [{
+            type: 'text',
+            text: "キーワード何にしようかな〜〜"
+          },{
+            type: 'text',
+            text: "#{seed1} × #{seed2} !!"
+          }]
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    }
+    head :ok
+  end
+  privete
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+  def select_word
+    seeds = ["アイデア１", "アイデア２", "アイデア３", "アイデア４"]
+    seeds.sample
+  end
+end
